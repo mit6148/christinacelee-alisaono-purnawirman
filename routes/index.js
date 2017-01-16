f21247c9a5b309ea2c5cbb3df99bb74e5dfa28b4
purnawirman purnawirman <purnawirman purnawirman>
2017-01-15 22:14:25 -0500

    did the routing, nodemon, and chalk

--- routes/index.js
+++ routes/index.js
@@ -1,83 +1,175 @@
 var express = require('express');
 var router = express.Router();
 
+// additional package
+var chalk = require('chalk');
+
 // get the User model
 var User = require('../schemas/user');
+
+
+// GET requests
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // Rendering the index view with the title 'Sign Up'
-  res.render('index', {title: 'Sign Up'});
+  res.render('home');
 });
 
-/* GET userlist JSON */
-router.get('/userlist', function(req, res, next) {
-  // Passing in an empty object to User.find() will return a list of
-  // all the users.
-  User.find({}, function(err, users) {
-    res.send(users);
-  });
+/* GET buddy search page */
+router.get('/buddy_search', function(req, res, next) {
+  // Rendering the index view with the title 'Sign Up'
+  res.render('buddy_search');
 });
 
-/* POST to adduser */
-router.post('/adduser', function(req, res, next) {
-  var username = req.body.username;
-  var userFruit = req.body.userfruit;
+/* GET tabi search page */
+router.get('/tabi_search', function(req, res, next) {
+  // Rendering the index view with the title 'Sign Up'
+  res.render('tabi_search');
+});
 
-  // TODO: Create a new document with the given username and favorite fruit.
-  // If the username already exists, then do nothing.
-  var newUser = new User({
-    'username': username,
-    'favoriteFruit': userFruit
-  });
-  newUser.save();
+/* GET view_user page */
+router.get('/view_user', function(req, res, next) {
+  // Rendering the index view with the title 'Sign Up'
+  res.render('user_profile');
+});
 
+/* GET add_trip_page */;
+router.get('/add_trip_page', function(req, res, next) {
+  // Rendering the index view with the title 'Sign Up'
+  res.render('add_trip');
+});
+
+/* GET edit_trip_page */
+router.get('/edit_trip_page', function(req, res, next) {
+  // Rendering the index view with the title 'Sign Up'
+  res.render('edit_trip');
+});
+
+
+/* POST like_trip*/
+router.post('/like_trip', function(req, res, next) {
+  var likedUser = req.body.user_id;
+  var likedTrip = req.body.trip_id;
+  console.log(chalk.red("Hit on like trip!"));
+
+  // TODO: need to redirect to the current page!
   // Redirecting back to the root
-  res.redirect('/');
+  res.redirect('/tabi_search')
 });
 
-/* POST to deleteuser */
-router.post('/deleteuser', function(req, res, next) {
-  var username = req.body.username;
+/* POST edit_profile_photo*/
+router.post('/edit_profile_photo', function(req, res, next) {
+  // var user_id = req.body.user_id;
+  // TODO: get photo
 
-  // TODO: Remove the document from the collection, if it exists.
-  // Otherwise, let the client know that the user does not exist.
-  //
-  // Hint: How can you tell whether User.remove() was successful?
-  // Look at the second parameter of the callback function passed
-  // User.remove(). You can get the number of documents deleted
-  // by the operation.
-  User.remove({'username': username}, function(err, result) {
-    if (err) {
-      res.send('An error occurred!');
-    } else {
-      if (result.result.n === 0) {
-        res.send(username + ' is not in the database!');
-      } else {
-        res.send(username + ' was successfully removed!');
-      }
-    }
-  });
+  // TODO: need to redirect to the current page!
+  // Redirecting back to the root
+  res.redirect('/view_user');
 });
 
-router.get('/findfruit', function(req, res, next) {
-  var username = req.query.username;
+/* POST edit_profile_text*/
+router.post('/edit_profile_text', function(req, res, next) {
+  // var user_id = req.body.user_id;
+  var new_text = req.body.new_profile_text;
 
-  // TODO: Check if the user exists. If the user exists, send back
-  // their favorite fruit. Otherwise, let the client know that the
-  // username is not in the database.
-  User.findOne({'username': username}, function(err, user) {
-    if (err) {
-      res.send('An error occurred!');
-    } else {
-      if (user) {
-        res.send(username + '\'s favorite fruit is ' + user.favoriteFruit);
-      } else {
-        // If the user does not exist, use this line of code below.
-        res.send(username + ' is not in the database!');
-      }
-    }
-  });
+  // TODO: need to redirect to the current page!
+  // Redirecting back to the root
+  res.redirect('/view_user');
 });
+
+/* POST add_trip*/
+router.post('/add_trip', function(req, res, next) {
+  // var user_id = req.body.user_id;
+  var user_id = req.body.user_id;
+  var title = req.body.title;
+  var description = req.body.description;
+  // TODO: photo
+
+  res.redirect('/add_trip');
+});
+
+/* POST edit_trip*/
+router.post('/edit_trip', function(req, res, next) {
+  // var user_id = req.body.user_id;
+  var trip_id = req.body.trip_id;
+  var title = req.body.title;
+  var description = req.body.description;
+  // TODO: photo
+
+  res.redirect('/user_profile');
+});
+
+/* POST delete_trip*/
+router.post('/delete_trip', function(req, res, next) {
+  // var user_id = req.body.user_id;
+  var trip_id = req.body.trip_id;
+  // TODO: photo
+
+  res.redirect('/user_profile');
+});
+
+
+// Example to add, edit and delete in mongoose
+// /* POST to adduser */
+// router.post('/adduser', function(req, res, next) {
+//   var username = req.body.username;
+//   var userFruit = req.body.userfruit;
+
+//   // TODO: Create a new document with the given username and favorite fruit.
+//   // If the username already exists, then do nothing.
+//   var newUser = new User({
+//     'username': username,
+//     'favoriteFruit': userFruit
+//   });
+//   newUser.save();
+
+//   // Redirecting back to the root
+//   res.redirect('/');
+// });
+
+// /* POST to deleteuser */
+// router.post('/deleteuser', function(req, res, next) {
+//   var username = req.body.username;
+
+//   // TODO: Remove the document from the collection, if it exists.
+//   // Otherwise, let the client know that the user does not exist.
+//   //
+//   // Hint: How can you tell whether User.remove() was successful?
+//   // Look at the second parameter of the callback function passed
+//   // User.remove(). You can get the number of documents deleted
+//   // by the operation.
+//   User.remove({'username': username}, function(err, result) {
+//     if (err) {
+//       res.send('An error occurred!');
+//     } else {
+//       if (result.result.n === 0) {
+//         res.send(username + ' is not in the database!');
+//       } else {
+//         res.send(username + ' was successfully removed!');
+//       }
+//     }
+//   });
+// });
+
+// router.get('/findfruit', function(req, res, next) {
+//   var username = req.query.username;
+
+//   // TODO: Check if the user exists. If the user exists, send back
+//   // their favorite fruit. Otherwise, let the client know that the
+//   // username is not in the database.
+//   User.findOne({'username': username}, function(err, user) {
+//     if (err) {
+//       res.send('An error occurred!');
+//     } else {
+//       if (user) {
+//         res.send(username + '\'s favorite fruit is ' + user.favoriteFruit);
+//       } else {
+//         // If the user does not exist, use this line of code below.
+//         res.send(username + ' is not in the database!');
+//       }
+//     }
+//   });
+// });
 
 module.exports = router;
