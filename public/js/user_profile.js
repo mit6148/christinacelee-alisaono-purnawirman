95712838b3c864d63d07720d9462f2146d984f96
Alisa Ono <alisao@mit.edu>
2017-01-16 21:12:50 -0500

    new: user_profile.js and user_profile_style.css; user_profile base structure with handlebars

---
+++ public/js/user_profile.js
@@ -0,0 +1,52 @@
+var main = function() {
+	
+	// loading fake data for front end testing
+
+	// var userTripsList = [{tripID: "12345", userID: "123", tripTitle: "test1", username: "user1", description: "this is test description1", liked: true, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12346", userID: "124", tripTitle: "test2", username: "user2", description: "this is test description2", liked:false, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12347", userID: "125", tripTitle: "test3", username: "user3", description: "this is test description3", liked:false, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12348", userID: "126", tripTitle: "test4", username: "user4", description: "this is test description4", liked:true, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12349", userID: "127", tripTitle: "test5", username: "user5", description: "this is test description5", liked:true, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12350", userID: "128", tripTitle: "test6", username: "user6", description: "this is test description6", liked: true, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12351", userID: "129", tripTitle: "test7", username: "user7", description: "this is test description7", liked:false, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12352", userID: "130", tripTitle: "test8", username: "user8", description: "this is test description8", liked:false, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12353", userID: "131", tripTitle: "test9", username: "user9", description: "this is test description9", liked:true, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12354", userID: "132", tripTitle: "test10", username: "user10", description: "this is test description10", liked:true, imageURL:'http://placekitten.com/g/150/150'},];
+
+	// var wishlistTripsList = [{tripID: "12345", userID: "123", tripTitle: "test1", username: "user1", description: "this is test description1", liked: true, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12346", userID: "124", tripTitle: "test2", username: "user2", description: "this is test description2", liked:false, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12347", userID: "125", tripTitle: "test3", username: "user3", description: "this is test description3", liked:false, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12348", userID: "126", tripTitle: "test4", username: "user4", description: "this is test description4", liked:true, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12349", userID: "127", tripTitle: "test5", username: "user5", description: "this is test description5", liked:true, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12350", userID: "128", tripTitle: "test6", username: "user6", description: "this is test description6", liked: true, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12351", userID: "129", tripTitle: "test7", username: "user7", description: "this is test description7", liked:false, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12352", userID: "130", tripTitle: "test8", username: "user8", description: "this is test description8", liked:false, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12353", userID: "131", tripTitle: "test9", username: "user9", description: "this is test description9", liked:true, imageURL:'http://placekitten.com/g/150/150'},
+	// {tripID: "12354", userID: "132", tripTitle: "test10", username: "user10", description: "this is test description10", liked:true, imageURL:'http://placekitten.com/g/150/150'},];
+
+	// var fakeData = {userImageURL: 'http://placekitten.com/g/150/150', username: "Cat Meow", userDescription: "this is test user description", userContact: 'test@gmail.com',
+	// wishlistTrips: wishlistTripsList, userTrips: userTripsList};
+
+	// var source = $("#hbtemplate").html();
+	// var template = Handlebars.compile(source);
+	// $('body').append(template(fakeData));
+
+ 	$('#trip-index').on('click',function(event){
+ 		event.preventDefault();
+ 		$('#trip-index').removeClass('inactive').addClass('active');
+ 		$('#trip-content').removeClass('inactive').addClass('active');
+ 		$('#wishlist-index').removeClass('active').addClass('inactive');
+ 		$('#wishlist-content').removeClass('active').addClass('inactive');
+ 	});
+
+ 	$('#wishlist-index').on('click',function(event){
+ 	 	event.preventDefault();
+ 		$('#wishlist-index').removeClass('inactive').addClass('active');
+ 		$('#wishlist-content').removeClass('inactive').addClass('active');
+ 		$('#trip-index').removeClass('active').addClass('inactive');
+ 		$('#trip-content').removeClass('active').addClass('inactive');
+ 	});
+
+ }
+
+ $(document).ready(main);
