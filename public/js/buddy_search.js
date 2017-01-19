ccd23ffde0bead1c3131efa6ac4dca01bea135f9
Alisa Ono <alisao@mit.edu>
2017-01-19 17:59:17 -0500

    filter bar (tabi & buddy search)

--- public/js/buddy_search.js
+++ public/js/buddy_search.js
@@ -1,6 +1,97 @@
+var buddySearchGroups = {
+  'Category': [
+    {title: 'Adventure', value: 'adventure'}, 
+    {title: 'Food', value: 'food'}, 
+    {title: 'Art', value: 'art'}, 
+    {title: 'History', value: 'history'}, 
+  ],
+  'Availability': [
+  {title: '2016 Winter', value: '2016Winter'}, 
+  {title: '2016 Fall', value: '2016Fall'}, 
+  {title: '2016 Summer', value: '2016Summer'}, 
+  {title: '2016 Spring', value: '2016Spring'},
+  ],
+  'Duration': [
+  {title: 'Day Trip', value: 'dayTrip'}, 
+  {title: 'Short(1~3 days)', value: 'shortTrip'}, 
+  {title: 'Medium(4~7 days)', value: 'mediumTrip'}, 
+  {title: 'Long(8+ days)', value: 'longTrip'},
+  ],
+  'Budget': [
+  {title: 'Low(~$300)', value: 'lowBudget'}, 
+  {title: 'Medium($300~800)', value: 'mediumBudget'}, 
+  {title: 'High($800~)', value: 'highBudget'}, 
+  ]    
+}
+
 var main = function() {
   // loadFakeTestingData();
+
+  populateSearchBar(buddySearchGroups);
+
+  $('#filter-by-container input[type=checkbox]').on('click',toggleFilterButtonTags);
+
+  $('#clear-filter').on('click',clearFilter);
+
   $('.user').on('click', showUserProfile);
+}
+
+function populateSearchBar(filter){
+  var filterForm = $('#filter-by-container form');
+  for (var group in filter){
+    $('<div>').attr('id',group+'-title-container').attr('rel',group).addClass('filter-title-container')
+      .mouseover(expandFilterCriteria).appendTo(filterForm);
+    $('<h2>').text(group).appendTo('#'+group+'-title-container');
+    $('<div>').attr('id',group+'-criteria-container').addClass('filter-criteria-container').appendTo(filterForm);
+
+    var criteria = filter[group];
+    for (i=0; i<criteria.length; i++){
+      var element = criteria[i];
+      $('#'+group+'-criteria-container').append(
+        '<input type="checkbox" name="'+group+'" value="'+element.value+'" title="'+element.title+'">'+element.title+'<br>'
+      );
+    }
+  }
+
+  $('<ul>').attr('id','filter-tags').appendTo(filterForm);
+  $('<button>').attr('id','apply-filter').text('Apply Filter').appendTo(filterForm);
+  $('<button>').attr('id','clear-filter').text('Clear Filter').appendTo(filterForm);
+}
+
+function expandFilterCriteria(event){
+  event.preventDefault();
+  var groupName = $(this).attr('rel');
+  $('.filter-criteria-container').hide();
+  $('#'+groupName+'-criteria-container').show();
+}
+
+function toggleFilterButtonTags(event){
+  if ($('#filter-by-container input[type=checkbox]:checked').length > 0) {
+    $('#filter-by-container button').show();
+    $('#filter-tags').show();
+  } else {
+    $('#filter-by-container button').hide(); 
+    $('#filter-tags').hide();
+  }
+
+  updateFilterTags();
+}
+
+function clearFilter(event){
+  event.preventDefault();
+  $('#filter-by-container input[type=checkbox]').prop('checked',false);
+  $('#filter-by-container button').hide();
+  $('#filter-tags').hide();
+
+  updateFilterTags();
+
+}
+
+function updateFilterTags() {
+  $('#filter-tags').children().remove();
+  $('#filter-by-container input[type=checkbox]:checked').each(function(){
+    $('<li>').text($(this).attr('title')).appendTo('#filter-tags');
+  })
 }
 
 function showUserProfile(event){
--- public/js/tabi_search.js
+++ public/js/tabi_search.js
@@ -3,14 +3,41 @@ var myUserID = 'tempUserID';
 var likeIconURL = "/images/like.png";
 var unlikeIconURL = "/images/unlike.png";
 
+var tripFilterGroups = {
+  'Category': [
+    {title: 'Adventure', value: 'adventure'}, 
+    {title: 'Food', value: 'food'}, 
+    {title: 'Art', value: 'art'}, 
+    {title: 'History', value: 'history'}, 
+  ],
+  'Season': [
+  {title: 'Winter', value: 'winter'}, 
+  {title: 'Fall', value: 'fall'}, 
+  {title: 'Summer', value: 'summer'}, 
+  {title: 'Spring', value: 'spring'},
+  ],
+  'Duration': [
+  {title: 'Day Trip', value: 'dayTrip'}, 
+  {title: 'Short(1~3 days)', value: 'shortTrip'}, 
+  {title: 'Medium(4~7 days)', value: 'mediumTrip'}, 
+  {title: 'Long(8+ days)', value: 'longTrip'},
+  ],
+  'Budget': [
+  {title: 'Low(~$300)', value: 'lowBudget'}, 
+  {title: 'Medium($300~800)', value: 'mediumBudget'}, 
+  {title: 'High($800~)', value: 'highBudget'}, 
+  ]    
+}
+
 var main = function() {
 
   //loadFakeTestingData();
 
-  // var windowHeight = window.innerHeight;
-  // var filterBarHeight = windowHeight - 70;
-  // console.log(windowHeight+','+filterBarHeight);
-  // $('#filter-by-container form').css('height',filterBarHeight+'px');
+  populateSearchBar(tripFilterGroups);
+
+  $('#filter-by-container input[type=checkbox]').on('click',toggleFilterButtonTags);
+
+  $('#clear-filter').on('click',clearFilter);
 
   $('.trip').on('click',function(event){
     event.preventDefault();
@@ -59,6 +86,64 @@ var main = function() {
     }
   });
 } 
+
+function populateSearchBar(filter){
+  var filterForm = $('#filter-by-container form');
+  for (var group in filter){
+    $('<div>').attr('id',group+'-title-container').attr('rel',group).addClass('filter-title-container')
+      .mouseover(expandFilterCriteria).appendTo(filterForm);
+    $('<h2>').text(group).appendTo('#'+group+'-title-container');
+    $('<div>').attr('id',group+'-criteria-container').addClass('filter-criteria-container').appendTo(filterForm);
+
+    var criteria = filter[group];
+    for (i=0; i<criteria.length; i++){
+      var element = criteria[i];
+      $('#'+group+'-criteria-container').append(
+        '<input type="checkbox" name="'+group+'" value="'+element.value+'" title="'+element.title+'">'+element.title+'<br>'
+      );
+    }
+  }
+
+  $('<ul>').attr('id','filter-tags').appendTo(filterForm);
+  $('<button>').attr('id','apply-filter').text('Apply Filter').appendTo(filterForm);
+  $('<button>').attr('id','clear-filter').text('Clear Filter').appendTo(filterForm);
+}
+
+function expandFilterCriteria(event){
+  event.preventDefault();
+  var groupName = $(this).attr('rel');
+  $('.filter-criteria-container').hide();
+  $('#'+groupName+'-criteria-container').show();
+}
+
+function toggleFilterButtonTags(event){
+  if ($('#filter-by-container input[type=checkbox]:checked').length > 0) {
+    $('#filter-by-container button').show();
+    $('#filter-tags').show();
+  } else {
+    $('#filter-by-container button').hide(); 
+    $('#filter-tags').hide();
+  }
+
+  updateFilterTags();
+}
+
+function clearFilter(event){
+  event.preventDefault();
+  $('#filter-by-container input[type=checkbox]').prop('checked',false);
+  $('#filter-by-container button').hide();
+  $('#filter-tags').hide();
+
+  updateFilterTags();
+
+}
+
+function updateFilterTags() {
+  $('#filter-tags').children().remove();
+  $('#filter-by-container input[type=checkbox]:checked').each(function(){
+    $('<li>').text($(this).attr('title')).appendTo('#filter-tags');
+  })
+}
 
 function showUserProfile(eventTarget){
   var userID = eventTarget.attr('rel');
