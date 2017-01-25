47c4347f7bb0772fa51222885674c6ca48c0dd8e
Alisa Ono <alisao@mit.edu>
2017-01-24 19:15:12 -0500

    top bar to add trip page

--- routes/index.js
+++ routes/index.js
@@ -120,7 +120,15 @@ router.get('/__database', function(req, res, next){
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', {showSearchBar: false, loggedIn: req.isAuthenticated()});
+  var loggedIn = req.isAuthenticated();
+  var username;
+  if (loggedIn) {
+    username = req.user.userName;
+  } else {
+    username = 'guest';
+  }
+
+  res.render('home', {showSearchBar: false, loggedIn: loggedIn, username: username});
 });
 
 router.get('/login/facebook', passport.authenticate('facebook'));
@@ -140,6 +148,14 @@ router.get('/logout', function(req, res, next) {
 /* GET buddy search result */
 // This GET req should return ALL buddies of the same location 
 router.get('/buddy_search', function(req, res, next) {
+
+  var loggedIn = req.isAuthenticated();
+  var username;
+  if (loggedIn) {
+    username = req.user.userName;
+  } else {
+    username = 'guest';
+  }
 
   var placeID = req.query.buddy_destination_id;
   var placeName = req.query.buddy_destination_name;
@@ -168,7 +184,7 @@ router.get('/buddy_search', function(req, res, next) {
           });
         }
       }
-      res.render('buddy_search', {users: buddyList, showSearchBar: false, loggedIn: req.isAuthenticated()});   
+      res.render('buddy_search', {users: buddyList, showSearchBar: false, loggedIn: loggedIn, username: username});   
     })
   });
 });
@@ -189,14 +205,21 @@ router.get('/buddy_search_filter', function(req, res, next) {
   tripImages: [{tripImageURL:'http://placekitten.com/g/150/150'},{tripImageURL:'http://placekitten.com/g/150/150'},{tripImageURL:'http://placekitten.com/g/150/150'},{tripImageURL:'http://placekitten.com/g/150/150'}]},
   {userID: "127", username: "user5", userImageURL: 'http://placekitten.com/g/150/150',
   tripImages: [{tripImageURL:'http://placekitten.com/g/150/150'},{tripImageURL:'http://placekitten.com/g/150/150'},{tripImageURL:'http://placekitten.com/g/150/150'},{tripImageURL:'http://placekitten.com/g/150/150'}]},
-  ],
-  showSearchBar: false, loggedIn: req.isAuthenticated()};
+  ]};
   res.render('buddy_search_result', fakeBuddyData);
 });
 
 /* GET tabi search result */
 // This GET req should return ALL trips of the same location 
 router.get('/tabi_search', function(req, res, next) {
+
+  var loggedIn = req.isAuthenticated();
+  var username;
+  if (loggedIn) {
+    username = req.user.userName;
+  } else {
+    username = 'guest';
+  }
 
   var placeID = req.query.tabi_destination_id;
   var placeName = req.query.tabi_destination_name;
@@ -251,8 +274,7 @@ router.get('/tabi_search_filter', function(req, res, next) {
   {tripID: "12346", userID: "124", tripTitle: "test2", username: "user2", description: "this is test description2", liked:false, imageURL:'http://placekitten.com/g/150/150'},
   {tripID: "12347", userID: "125", tripTitle: "test3", username: "user3", description: "this is test description3", liked:false, imageURL:'http://placekitten.com/g/150/150'},
   {tripID: "12348", userID: "126", tripTitle: "test4", username: "user4", description: "this is test description4", liked:true, imageURL:'http://placekitten.com/g/150/150'},
-  {tripID: "12349", userID: "127", tripTitle: "test5", username: "user5", description: "this is test description5", liked:true, imageURL:'http://placekitten.com/g/150/150'},],
-  showSearchBar: false, loggedIn: req.isAuthenticated()};
+  {tripID: "12349", userID: "127", tripTitle: "test5", username: "user5", description: "this is test description5", liked:true, imageURL:'http://placekitten.com/g/150/150'},],};
 
   res.render('tabi_search_result', fakeTripData);
 });
@@ -268,15 +290,22 @@ router.get('/view_my_profile', function(req, res, next) {
 
 /* GET user profile page by user ID */
 router.get('/view_user/:user_id', function(req, res, next) {
-
   // userID is user ID of the profile we are accessing
   var userID = req.params.user_id;
 
   // req.user.userID is user ID of the person who is viewing the profile
   // set userIsOwner to true, if the person is viewing his/her own profile
   var userIsOwner = false;
-  if (userID === req.user.userID) {
-    userIsOwner = true;
+  
+  var loggedIn = req.isAuthenticated();
+  var username;
+  if (loggedIn) {
+    username = req.user.userName;
+    if (userID === req.user.userID) {
+      userIsOwner = true;
+    }
+  } else {
+    username = 'guest';
   }
 
   // var yourOwnName = req.user.userName;
@@ -324,7 +353,15 @@ router.get('/view_user/:user_id', function(req, res, next) {
 /* GET add trip page */;
 router.get('/add_trip_page', function(req, res, next) {
 
-  res.render('add_trip_temp');
+  var loggedIn = req.isAuthenticated();
+  var username;
+  if (loggedIn) {
+    username = req.user.userName;
+  } else {
+    username = 'guest';
+  }
+
+  res.render('add_trip_temp', {loggedIn: loggedIn, username: username});
 });
 
 /* GET edit trip page by trip ID */
